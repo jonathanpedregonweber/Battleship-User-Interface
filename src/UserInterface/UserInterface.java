8b9ef5cdbbce06b4ddaa392dd286d4cd75fab67f
jonathanpedregon <jonathanpedregon@gmail.com>
2018-08-16 15:37:07 -0600

    Adding buttons to opponent panel.

--- src/UserInterface/UserInterface.java
+++ src/UserInterface/UserInterface.java
@@ -5,14 +5,44 @@ import java.awt.*;
 
 public class UserInterface extends JFrame
 {
-    private JButton[][] EnemyButtons = new JButton[10][10];
+    private JButton[][] OpponentButtons = new JButton[10][10];
+    private JPanel OpponentPanel;
 
     public UserInterface()
     {
         JPanel mainPanel = new JPanel(new GridLayout(2,2));
+        GetOpponentPanel();
+        mainPanel.add(OpponentPanel);
         setName("Battleship User Interface");
         setSize(1500,900);
         setContentPane(mainPanel);
         setVisible(true);
+    }
+
+    private void GetOpponentPanel()
+    {
+        OpponentPanel = new JPanel(new GridLayout(10,10));
+        OpponentButtons = CreateButtons();
+    }
+
+    private JButton[][] CreateButtons()
+    {
+        JButton[][] buttonArray = new JButton[10][10];
+        for(int row = 0; row < 10; row++)
+        {
+            for(int column = 0; column < 10; column++)
+            {
+                JButton opponentButton = new JButton();
+                opponentButton.setText("A");
+                OpponentPanel.add(opponentButton);
+                buttonArray[row][column] = new JButton();
+            }
+        }
+        return buttonArray;
+    }
+
+    private void AddOpponentButtonsToPanel(JPanel panel)
+    {
+
     }
 }
