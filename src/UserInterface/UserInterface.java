b85379203ecb777c0ddd8a903eef17c7a66abe70
jonathanpedregon <jonathanpedregon@gmail.com>
2018-08-16 15:57:26 -0600

    Adding user panel

--- src/UserInterface/UserInterface.java
+++ src/UserInterface/UserInterface.java
@@ -1,31 +1,41 @@
 package UserInterface;
 
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class UserInterface extends JFrame
 {
     private JButton[][] OpponentButtons = new JButton[10][10];
     private JPanel OpponentPanel;
+    private JButton[][] UserButtons = new JButton[10][10];
+    private JPanel UserPanel;
 
     public UserInterface()
     {
-        JPanel mainPanel = new JPanel(new GridLayout(2,2));
+        JPanel boardPanel = new JPanel(new GridLayout(1,2));
+        JPanel mainPanel = new JPanel(new GridLayout(2,1));
         GetOpponentPanel();
-        mainPanel.add(OpponentPanel);
+        GetUserPanel();
+        boardPanel.add(OpponentPanel);
+        boardPanel.add(UserPanel);
+        mainPanel.add(boardPanel);
+
         setName("Battleship User Interface");
         setSize(1500,900);
         setContentPane(mainPanel);
         setVisible(true);
     }
 
-    private void GetOpponentPanel()
+    private void GetUserPanel()
     {
-        OpponentPanel = new JPanel(new GridLayout(10,10));
-        OpponentButtons = CreateButtons();
+        UserPanel = new JPanel(new GridLayout(10,10));
+        UserPanel.setBorder(new LineBorder(Color.BLUE, 7));
+        UserButtons = CreateUserButtons();
     }
 
-    private JButton[][] CreateButtons()
+    private JButton[][] CreateUserButtons()
     {
         JButton[][] buttonArray = new JButton[10][10];
         for(int row = 0; row < 10; row++)
@@ -33,7 +43,30 @@ public class UserInterface extends JFrame
             for(int column = 0; column < 10; column++)
             {
                 JButton opponentButton = new JButton();
-                opponentButton.setText("A");
+                opponentButton.setText(GetButtonText(column, row));
+                UserPanel.add(opponentButton);
+                buttonArray[row][column] = new JButton();
+            }
+        }
+        return buttonArray;
+    }
+
+    private void GetOpponentPanel()
+    {
+        OpponentPanel = new JPanel(new GridLayout(10,10));
+        OpponentPanel.setBorder(new LineBorder(Color.BLUE, 7));
+        OpponentButtons = CreateOpponentButtons();
+    }
+
+    private JButton[][] CreateOpponentButtons()
+    {
+        JButton[][] buttonArray = new JButton[10][10];
+        for(int row = 0; row < 10; row++)
+        {
+            for(int column = 0; column < 10; column++)
+            {
+                JButton opponentButton = new JButton();
+                opponentButton.setText(GetButtonText(column, row));
                 OpponentPanel.add(opponentButton);
                 buttonArray[row][column] = new JButton();
             }
@@ -41,8 +74,9 @@ public class UserInterface extends JFrame
         return buttonArray;
     }
 
-    private void AddOpponentButtonsToPanel(JPanel panel)
+    private String GetButtonText(int columnIndex, int rowIndex)
     {
-
+        String[] letters = {"A","B","C","D","E","F","G","H","I","J"};
+        return letters[columnIndex] + (rowIndex + 1);
     }
 }
